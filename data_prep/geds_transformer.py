628633d5703366e3514357d18f32a0bd0db49564
William Wolfe-Wylie <william@wolfewylie.com>
2023-03-03 11:15:05 -0500

    Getting organized with a real solution

---
+++ data_prep/geds_transformer.py
@@ -0,0 +1,22 @@
+import json
+import re 
+
+filename = "gedsOpenData.json"
+gedsData = ""
+
+with open(filename, 'r') as fileObject: 
+    gedsData = fileObject.read()
+
+newGEDSFile = []
+
+# Just REGEX our way through the text file to identify the individual people and save them as a list
+for every_person in re.finditer("\{(.+?)\}\n", gedsData, re.DOTALL):
+    new_person = every_person.group(0)
+    newJson = json.loads(new_person)
+    newGEDSFile.append(newJson)
+
+print(len(newGEDSFile), " records found.")
+
+#Save that new list to a proper file
+with open('json_files/updatedGEDS.json', 'w') as outfile:
+    json.dump(newGEDSFile, outfile)
---
+++ data_prep/oneDrive_reader.py
@@ -0,0 +1,30 @@
+from seleniumwire import webdriver  # Import from seleniumwire
+from seleniumwire.utils import decode
+import time
+import json
+
+#This code largely drawn from here: https://pypi.org/project/selenium-wire/#all-options
+
+# Create a new instance of the Chrome driver
+driver = webdriver.Chrome()
+
+downloadURLs = {}
+
+# Go to the page
+driver.get('https://onedrive.live.com/?cid=994cdaa3eee88fea&id=994CDAA3EEE88FEA%21106&authkey=!ACpKRkvFbmjf-PA')
+
+#This is awkward, but you have to manually scroll down the page to load all items. 
+#To Do -- automate this bit of scrolling after load.
+time.sleep(10)
+
+# Access requests via the `requests` attribute
+for request in driver.requests:
+    if "children" in request.url:
+        response_body = json.loads(decode(request.response.body, request.response.headers.get('Content-Encoding', 'identity')))
+        for every_file in response_body['value']:
+            deptName = every_file['name'].replace('.json', '')
+            deptDownloadURL = every_file['@content.downloadUrl']
+            downloadURLs[deptName] = deptDownloadURL
+
+with open('json_files/OneDrivedataLinks.json', 'w') as outfile:
+    json.dump(downloadURLs, outfile)
